de2019cea2ecf4c785ca081611522760645ad1fb
J <jiegu1989@gmail.com>
2019-02-11 19:50:51 -0500

    Working without args

--- project2_img_classifier/predict.py
+++ project2_img_classifier/predict.py
@@ -8,14 +8,17 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from PIL import Image
 import argparse
-
+from collections import OrderedDict
+from process import process_image
+import json
 
 
 
 def load_model(path):
-    chp = torch.load(path)
-
+    checkpoint = torch.load(path)
+#     checkpoint = torch.load(path, map_location=lambda storage, loc: storage)
     model = models.densenet121()
+    print(model)
     classifier = nn.Sequential(OrderedDict([
         ('fc1', nn.Linear(1024, 500)),
         ('relu', nn.ReLU()),
@@ -45,33 +48,39 @@ def predict(image_path, model, topk, device):
     probs = torch.nn.functional.softmax(outputs.data)
     return probs.topk(topk)
 
-def plot(image_path):
+def plot(image_path, cat_to_name, device = 'cpu'):
     # image_path = 'flowers/test/28/image_05230.jpg'
     # image_path = 'flowers/test/1/image_06743.jpg'
 
     im = Image.open(image_path)
-    probs, idx = predict(image_path, model, 5, 'cuda')
+    probs, idx = predict(image_path, model, 5, device)
     idx_to_class = {v: k for k, v in class_to_idx.items()}
     top_class = [idx_to_class[x] for x in idx.tolist()[0]]
     names = [cat_to_name[k] for k in top_class]
-    # print(names)
-    # print(probs)
+    print(names)
+    print(probs)
 
     f, (ax1, ax2) = plt.subplots(figsize=(8, 8), ncols=1, nrows=2)
     ax1.imshow(im)
     # ax1.imshow(image_path)
     ax2.barh(names, probs.tolist()[0])
 
-# parser = argparse.ArgumentParser(description='Predict the class')
-# parser.add_argument('input', type=str, metavar='', help='Path of image')
-# parser.add_argument('chp', type=str, metavar='', help='Path of checkpoint')
-# parser.add_argument('--top_k', type=int, metavar='', required=True, help='Top K most likely classes')
-# parser.add_argument('--category_names', type=str, metavar='', help='Use a mapping of categories to real names')
-# parser.add_argument('--gpu', metavar='', help='Use GPU')
-#
-# args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Predict the class')
+parser.add_argument('input', type=str, metavar='', help='Path of image')
+parser.add_argument('chp', type=str, metavar='', help='Path of checkpoint')
+parser.add_argument('--top_k', type=int, metavar='', required=True, help='Top K most likely classes')
+parser.add_argument('--category_names', type=str, metavar='', help='Use a mapping of categories to real names')
+parser.add_argument('--gpu', metavar='', help='Use GPU')
+
+args = parser.parse_args()
 
 
-% load model
-model = load_model('checkpoint.pth')
-% make predictions
+# load model
+model, class_to_idx = load_model('checkpoint.pth')
+
+
+# make predictions
+with open('cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+image_path = 'flowers/test/28/image_05230.jpg'
+plot(image_path, cat_to_name, 'cuda')
